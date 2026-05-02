f8747e61082a7e52cd3b3cdc0360930371e262e7
Chabelabela <szalai.david100@gmail.com>
2026-05-02 10:21:54 +0200

    RESTART BABBBYYY

---
+++ src/main/java/org/chabelabela/outer_crafts_neo/Config.java
@@ -0,0 +1,6 @@
+package org.chabelabela.outer_crafts_neo;
+
+// This file is intentionally left empty.
+// The Config class was part of the NeoForge template boilerplate
+// and has been removed as part of the Outer Crafts port.
+public class Config {}
---
+++ src/main/java/org/chabelabela/outer_crafts_neo/Outer_crafts_neo.java
@@ -0,0 +1,6 @@
+package org.chabelabela.outer_crafts_neo;
+
+// This file is intentionally left empty.
+// The mod entry point has been moved to org.chabelabela.outer_crafts.OuterCrafts.
+// The class must remain here to satisfy the original source root structure.
+public class Outer_crafts_neo {}
